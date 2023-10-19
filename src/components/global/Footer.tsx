ef045b25dfdf8ed02d79555ac339cb69ff2d0ad0
Lakmi <lakmi94@gmail.com>
2023-10-19 15:14:51 +0900

    Feat: add footer

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -1,8 +1,24 @@
-import { Flex } from "@chakra-ui/react";
-
+import { Flex,Text } from "@chakra-ui/react";
+import dayjs from "dayjs";
+ import TopArrow from '@/assets/images/top_arrow_icon.svg'
+import Image from "next/image";
+import { theme } from "@/theme";
 const Footer = () => {
+    const scrollToTop = () => {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      };
+    
+
     return (
-        <Flex>Footer</Flex>
+        <Flex height={'78px'} justifyContent={'space-between'} alignItems={'center'} pl='30px' pr={'35px'}>
+            <Text fontFamily={'theme.fonts.openSans'} color={'#9A9AAF'} fontSize={'16px'} fontWeight={600} > ©  {dayjs().year()}{' '}Tokamak Network</Text>
+            <Flex height={'32px'} width={'20px'} onClick={scrollToTop} _hover={{ cursor: "pointer" }}>
+                <Image src={TopArrow} alt="scroll to top"/>
+            </Flex>
+        </Flex>
     )
    
 }
