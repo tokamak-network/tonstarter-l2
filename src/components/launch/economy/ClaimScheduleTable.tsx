d596b409739d3960d3946964b10c6be28483be08
Lakmi <lakmi94@gmail.com>
2023-11-14 17:59:28 +0900

    Add Claim table

--- src/components/launch/economy/ClaimScheduleTable.tsx
+++ src/components/launch/economy/ClaimScheduleTable.tsx
@@ -38,63 +38,128 @@ const ClaimScheduleTable = () => {
             <chakra.tr
               {...headerGroup.getFooterGroupProps()}
               key={index}
-              
               borderBottom={"1px solid #313442"}
-              height={"41px"}>
+              mb={"12px"}>
               {headerGroup.headers.map((column, index) => {
-                console.log("column", column);
                 return (
-                  <chakra.th
-                  
-                    {...column.getHeaderProps()}
-                    key={index}
-                   
-                   >
-                    <Flex  mb={"9px"} flexDir={'column'}  height={"32px"}>
-                    <Text
-                      height={"12px"}
-                      borderRight={ column.id === "TokenAllocation" ? '':"1px solid #484848"}
-                      display={"flex"}
+                  <chakra.th {...column.getHeaderProps()} key={index}>
+                    <Flex
+                      mb={"9px"}
+                      flexDir={"column"}
+                      height={"32px"}
                       justifyContent={"center"}
-                      alignItems={"center"}
-                      width={
-                        column.id === "id"
-                          ? "50px"
-                          : column.id === "DateTime"
-                          ? "128px"
-                          : "160px"
-                      }
-                      color={"#fff"}
-                      fontSize={"13px"}>
-                      {column.render("Header")}
-                    </Text>
-                   { column.id === "TokenAllocation" ?<Text fontSize={'13px'} fontWeight={400} color={'#9d9ea5'}>(Accumulated)</Text>:<></>}
+                      alignItems={"center"}>
+                      <Text
+                        height={"12px"}
+                        borderRight={
+                          column.id === "TokenAllocation"
+                            ? ""
+                            : "1px solid #484848"
+                        }
+                        display={"flex"}
+                        justifyContent={"center"}
+                        alignItems={"center"}
+                        width={
+                          column.id === "id"
+                            ? "50px"
+                            : column.id === "DateTime"
+                            ? "128px"
+                            : "160px"
+                        }
+                        color={"#fff"}
+                        fontSize={"13px"}>
+                        {column.render("Header")}
+                      </Text>
+                      {column.id === "TokenAllocation" ? (
+                        <Text
+                          fontSize={"13px"}
+                          fontWeight={400}
+                          color={"#9d9ea5"}>
+                          (Accumulated)
+                        </Text>
+                      ) : (
+                        <></>
+                      )}
                     </Flex>
-                  
-
                   </chakra.th>
                 );
               })}
             </chakra.tr>
           ))}
         </chakra.thead>
-        <chakra.tbody {...getTableBodyProps()}>
-          {rows.map((row, index) => {
-            prepareRow(row);
-            return (
-              <chakra.tr {...row.getRowProps()} key={index}>
-                {row.cells.map((cell, index) => {
-                  console.log("cell", cell.row);
+        <chakra.tbody {...getTableBodyProps()}   mt={'12px !important'}>
+         
+            {rows.map((row, index) => {
+              prepareRow(row);
+              return (
+                <chakra.tr
+                  {...row.getRowProps()}
+                  key={index}
 
-                  return (
-                    <chakra.td {...cell.getCellProps()} key={index}>
-                      {cell.render("Cell")}
-                    </chakra.td>
-                  );
-                })}
-              </chakra.tr>
-            );
-          })}
+                
+                  height={"50px"}
+                  justifyContent={"center"}
+                  alignItems={"center"}>
+                  {row.cells.map((cell, index) => {
+                    const type = cell.column.id;
+                    const amount = Number(cell.row.original.TokenAllocation);
+
+                    return (
+                      <chakra.td
+                        {...cell.getCellProps()}
+                        key={index}
+                        
+                        height={'50px'}
+                        >
+                        <Flex
+                          w={
+                            type === "id"
+                              ? "50px"
+                              : type === "DateTime"
+                              ? "128px"
+                              : "160px"
+                          }
+                          borderRight={
+                            type === "TokenAllocation"
+                              ? ""
+                              : "1px solid #484848"
+                          }
+                          flexDir={"column"}
+                        height={'12px'}
+                          justifyContent={"center"}
+                          alignItems={"center"}>
+                          <Text
+                            display={"flex"}
+                            justifyContent={"center"}
+                            alignItems={"center"}
+                          
+                            w={"100%"}
+                          
+                            color={"#fff"}
+                            letterSpacing={"0.26px"}
+                            fontSize={"13px"}
+                            fontWeight={400}>
+                            {type === "id" ? "0" : ""}
+                            {cell.render("Cell")}
+                          </Text>
+                          <Text
+                            fontSize={"13px"}
+                            fontWeight={400}
+                            color={"#9d9ea5"}>
+                            {type === "TokenAllocation"
+                              ? "603232"
+                              : type === "DateTime"
+                              ? "(UTC+9)"
+                              : ""}
+                          </Text>
+                        </Flex>
+                      </chakra.td>
+                    );
+                  })}
+                </chakra.tr>
+              );
+            })}
+         
         </chakra.tbody>
       </chakra.table>
     </Flex>
