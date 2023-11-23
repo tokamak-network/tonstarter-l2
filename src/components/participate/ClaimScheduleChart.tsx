382f7c233464c331b88493d788832e60dd296d61
Lakmi <lakmi94@gmail.com>
2023-11-23 18:00:12 +0900

    temp

--- src/components/participate/ClaimScheduleChart.tsx
+++ src/components/participate/ClaimScheduleChart.tsx
@@ -1,12 +1,17 @@
 import { Flex, Text } from "@chakra-ui/react";
 
 const ClaimScheduleChart = () => {
+  return (
+    <Flex mt={"90px"}>
+      <Text
+        color={"#fff"}
+        fontSize={"24px"}
+        fontWeight={700}
+        lineHeight={"21px"}>
+        Claim schedule
+      </Text>
+    </Flex>
+  );
+};
 
-    return (
-        <Flex mt={'90px'}>
-           <Text> Claim schedule</Text>
-        </Flex>
-    )
-}
-
-export default ClaimScheduleChart
+export default ClaimScheduleChart;
